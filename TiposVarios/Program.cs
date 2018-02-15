f5ceafbac79e9fa0a849d6b2eea69cbf43a339ac
Keithuu <tonny_ctl@hotmail.com>
2018-02-15 11:14:44 +0000

    Lowkey figuring out the small differences and noticing its more JS than actual C imo.

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,6 +10,21 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            ushort banana = 120;
+            ushort apple = 3000;
+            int diff = apple - banana;
+            bool verify = true;
+            char trademark = '\u0025';
+            Console.WriteLine("Banana = " + banana);
+            Console.WriteLine("Apple = " + apple);
+            Console.WriteLine("banana - apple = " + diff);
+            Console.WriteLine("Another way to do banana - apple = " + (apple - banana) + trademark);
+            if(verify == true || banana == 1)
+            {
+                Console.WriteLine("IT WOOOOOOOORKED!");
+            }
+            Console.ReadKey();
         }
     }
 }
+    
